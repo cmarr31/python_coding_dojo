4d4758216a7ea62960a8cb288c050d47a7710260
cmarr31 <chris@sitefuelusa.com>
2017-05-09 15:12:39 -0500

    Compare arrays challenge.

---
+++ compare_arrays.py
@@ -0,0 +1,8 @@
+def compare(lst_one, lst_two):
+	if (lst_one == lst_two):
+		print "The lists are the same."
+	else:
+		print "The lists are NOT the same."
+
+compare([1,2,5,6,2], [1,2,5,6,2])
+compare([1,6,2], [1,2,5,6,2])
