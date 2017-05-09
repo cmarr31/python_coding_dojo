c3205a31dbe36ec2f467ad9f6df3f6fd337d528c
cmarr31 <chris@sitefuelusa.com>
2017-05-09 12:10:00 -0500

    Wrote min and max function.

--- string_and_list_practice.py
+++ string_and_list_practice.py
@@ -12,3 +12,30 @@ def find_and_replace(str):
 find_and_replace("It's thanksgiving day. It's my birthday,too!")
 
 # str.replace(old, new[, max])
+
+def min_and_max(lst):
+	print min(lst)
+	print max(lst)
+
+
+#	count = 0
+#	while count < len(lst):
+#		if lst[i][count]<lst[i][count + 1]:
+#			min = lst[i]
+#			count +=1
+#			print min
+#		if lst[i][count]>lst[i][count + 1]:
+#			max = lst[i]
+#			count +=1
+#			print max
+#	if([item for item in lst if lst[count] < lst[count + 1]]):
+#		min = lst[count]
+#		count +=1
+#		print min
+#	if([item for item in lst if lst[count] > lst[count + 1]]):
+#		max = lst[count]
+#		count +=1
+#		print max
+
+
+min_and_max([1, 5, 34, 8])
