37c5d78ee17367c79770d5aca5f518a17bccdfda
cmarr31 <chris@sitefuelusa.com>
2017-05-09 13:57:31 -0500

    Finish challenge.

--- string_and_list_practice.py
+++ string_and_list_practice.py
@@ -37,5 +37,74 @@ def min_and_max(lst):
 #		count +=1
 #		print max
 
+z = [1, 5, 34, 8, -3, 3, 6, 55]
 
-min_and_max([1, 5, 34, 8])
+min_and_max(z)
+
+
+def first_and_last(lst):
+	print lst[0]
+	print lst[-1]
+	lst.sort()
+	print lst
+
+first_and_last(z)
+
+import math
+
+def new_list(lst):
+	lst.sort()
+	half_way_point = len(lst)
+	half_way_point /=2
+	print half_way_point
+
+	print "---------"
+
+	#math.floor(half_way_point)
+	#half_way_point -=1
+
+	floor_num = math.floor(half_way_point)
+	array = []
+
+	for i in range(0, int(floor_num)):
+		array.append(i)
+	lst.insert(0, array)
+	print lst
+
+print "--------------"
+
+new_list(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
